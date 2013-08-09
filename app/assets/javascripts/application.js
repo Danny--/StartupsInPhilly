60bdb5e45c020e644ad7eef19d3e4601cc794619
D <dannyqho@gmail.com>
2013-08-09 06:40:18 -0400

    got a map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,18 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJtfksrR5F_ur0XMbAYGhvKrQsJ7g2loA&sensor=SFALSE"
+
+function initialize() {
+  var mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
